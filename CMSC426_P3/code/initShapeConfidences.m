7357750fcb2ec6162120673a5d39df01006b93a8
Sabrina Zhou <szhou125@terpmail.umd.edu>
2019-11-11 17:21:45 -0500

    global affine

--- CMSC426_P3/code/initShapeConfidences.m
+++ CMSC426_P3/code/initShapeConfidences.m
@@ -1,4 +1,4 @@
 function ShapeConfidences = initShapeConfidences(LocalWindows, ColorConfidences, WindowWidth, SigmaMin, A, fcutoff, R)
 % INITSHAPECONFIDENCES Initialize shape confidences.  ShapeConfidences is a struct you should define yourself.
-
+    
 end
