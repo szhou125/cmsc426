57ad2df83d5959e9a52bf112db6b72f2c93397a3
Sabrina Zhou <szhou125@terpmail.umd.edu>
2019-11-18 22:38:09 -0500

    almost there

--- CMSC426_P3/code/updateModels.m
+++ CMSC426_P3/code/updateModels.m
@@ -24,19 +24,21 @@ function [mask, LocalWindows, ColorModels, ShapeConfidences] = ...
     confidence = cell(numLocalWindows, 1);
     foreground = cell(numLocalWindows, 1);
     background = cell(numLocalWindows, 1);
-    windows_prob = cell(numLocalWindows, 1);
     f_prob = cell(numLocalWindows, 1);
     b_prob = cell(numLocalWindows, 1);
-    distance = cell(numLocalWindows, 1);
-    prob = cell(numLocalWindows, 1);
+    distances = cell(numLocalWindows, 1);
+    probs = cell(numLocalWindows, 1);
+    w = cell(numLocalWindows, 1);
 
     %need to update cells
     %can i condense this since its in 1 file
     %need to really understand what im doing lol
     %what are the fields of each?
+    
+    img = rgb2lab(CurrentFrame);
+
     for i = 1:size(NewLocalWindows, 1)
         
-        img = rgb2lab(CurrentFrame);
         
         %gets corners of window
         lowerX = round(LocalWindows(i,1) - WindowWidth / 2);
@@ -44,7 +46,7 @@ function [mask, LocalWindows, ColorModels, ShapeConfidences] = ...
         lowerY = round(LocalWindows(i,2) - WindowWidth / 2);
         upperY = round(LocalWindows(i,2) + WindowWidth / 2); 
         
-        window = img( lowerY:upperY,lowerX:upperX,:);
+        window = img(lowerY:upperY,lowerX:upperX,:);
         
         l = window(:,:,1);
         a = window(:,:,2);
@@ -52,8 +54,10 @@ function [mask, LocalWindows, ColorModels, ShapeConfidences] = ...
 
         restructuredPoints(:,1:3) = [l(:),a(:),b(:)];
         
+        %for each point in restructured points
         curr_window_f_prob = pdf(ColorModels.fGMM{i}, restructuredPoints);
         curr_window_b_prob = pdf(ColorModels.bGMM{i}, restructuredPoints);
+        
         
         old_probs = curr_window_f_prob ./ (curr_window_f_prob + curr_window_b_prob);
         
@@ -75,9 +79,9 @@ function [mask, LocalWindows, ColorModels, ShapeConfidences] = ...
         new_probs = new_window_f_prob ./ (new_window_f_prob + new_window_b_prob);
 
         %just gets indices
-        new_f_count = find(old_probs == 1);
+        new_f_count = find(new_probs == 1);
         %can i rewrite this lol like total - f count or something?
-        new_b_count = find(old_probs ~= 1);  
+        new_b_count = find(new_probs ~= 1);  
         
         NewDistance = bwdist(warpedMaskOutline);
         NewDistance = NewDistance( lowerY:upperY, lowerX:upperX);
@@ -101,14 +105,36 @@ function [mask, LocalWindows, ColorModels, ShapeConfidences] = ...
             prob = reshape(new_probs, [size(window, 1) size(window, 2)]);
             
         else
-            %set new confidence, fgmm, bgmm, fpx, bpx, newprob
-            
+            %set confidence, fgmm, bgmm, fpx, bpx, newprob
+            newfGMM = ColorModels.fGMM{i};
+            newbGMM = ColorModels.bGMM{i};
+            conf = ColorModels.Confidence{i};
+            newFPoints = f_probs;
+            newBPoints = b_probs
+            prob = reshape(old_probs, [size(window, 1) size(window, 2)]);
         end
-        
+        w{i} = NewLocalWindows(i,:);
+        confidence{i} = conf;
+        foreground{i} = newFPoints;
+        background{i} = newBPoints;
+        f_prob{i} = newfGMM;
+        b_prob{i} = newbGMM;
+        distances{i} = NewDistance;
+        probs{i} = prob;
     end
     
+    newColorModel = struct;
+    newColorModel.Confidence = confidence;
+    newColorModel.bGMM = b_prob;
+    newColorModel.fGMM = f_prob;
+    newColorModel.Windows = w;
+    newColorModel.fPoints = foreground;
+    newColorModel.bPoints = background;
+    newColorModel.distance = distances;
+    newColorModel.prob = probs;
+
     %uses the new color models
-    ShapeConfidences = initShapeConfidences(NewLocalWindows, ColorModels, WindowWidth, SigmaMin, A, R, fcutoff);
+    ShapeConfidences = initShapeConfidences(NewLocalWindows, newColorModel, WindowWidth, SigmaMin, A, R, fcutoff);
     
     %is the x and y switched?
     LocalWarpedMasks = cell(numLocalWindows, 1);
@@ -117,7 +143,7 @@ function [mask, LocalWindows, ColorModels, ShapeConfidences] = ...
         upperX = round(NewLocalWindows(i,1) + WindowWidth / 2);
         lowerY = round(NewLocalWindows(i,2) - WindowWidth / 2);
         upperY = round(NewLocalWindows(i,2) + WindowWidth / 2); 
-        LocalWarpedMasks{i} = warpedMask(lowerX:upperX, lowerY:upperY);
+        LocalWarpedMasks{i} = warpedMask( lowerY:upperY, lowerX:upperX);
     end
     
     pfs = cell(numLocalWindows, 1);
@@ -125,7 +151,7 @@ function [mask, LocalWindows, ColorModels, ShapeConfidences] = ...
         localMask = LocalWarpedMasks{i};
         shapeConf = ShapeConfidences.Confidence{i}
         %what does colormodels have?
-        pfs{i} = shapeConf.*localMask + (1 - shapeConf) .* ColorModels.prob{i};
+        pfs{i} = shapeConf.*localMask + (1 - shapeConf) .* newColorModel.prob{i};
     end
     
     %merging
@@ -139,13 +165,13 @@ function [mask, LocalWindows, ColorModels, ShapeConfidences] = ...
         lowerY = round(NewLocalWindows(i,2) - WindowWidth / 2);
         upperY = round(NewLocalWindows(i,2) + WindowWidth / 2); 
         
-        for row = lowerX:upperX
-            for col = lowerY:upperY
-                denom = inv(pdist([NewLocalWindows(i,1), NewLocalWindows(i,1); col, row]) + 0.1);
+        for row = lowerY:upperY
+            for col = lowerX:upperX
+                denom = inv(pdist([NewLocalWindows(i,1), NewLocalWindows(i,2); col, row]) + 0.1);
                 %switch x and y?
-                num = pfsk( col - lowerY + 1, row - lowerX + 1) * denom;
-                maskNum(row, col) = maskNum(row, col) + num;
-                maskDenom(row, col) = maskDenom(row, col) + denom;
+                num = pfsk( col - lowerX + 1, row - lowerY + 1) * denom;
+                maskNum(row, col) = maskNum(row,col) + num;
+                maskDenom(row, col) = maskDenom(row,col) + denom;
             end
         end  
     end
@@ -158,14 +184,13 @@ function [mask, LocalWindows, ColorModels, ShapeConfidences] = ...
     UpdatedMask = zeros(size(Pfs));
     UpdatedMask(Pfs > ProbMaskThreshold) = 1;
     for i = 1:size(LocalWindows, 1)
-        lowerX = round(LocalWindows(i,2) - WindowWidth / 2);
-        upperX = round(LocalWindows(i,2) + WindowWidth / 2);
-        lowerY = round(LocalWindows(i,1) - WindowWidth / 2);
-        upperY = round(LocalWindows(i,1) + WindowWidth / 2); 
-        disp(lowerX)
-        window = UpdatedMask(lowerX:upperX,lowerY:upperY);
+        lowerY = round(LocalWindows(i,2) - WindowWidth / 2);
+        upperY = round(LocalWindows(i,2) + WindowWidth / 2);
+        lowerX = round(LocalWindows(i,1) - WindowWidth / 2);
+        upperX = round(LocalWindows(i,1) + WindowWidth / 2); 
+        window = UpdatedMask(lowerY:upperY, lowerX:upperX);
         window = imfill(window, 'holes');
-        UpdatedMask( lowerX:upperX,lowerY:upperY) = window;
+        UpdatedMask( lowerY:upperY, lowerX:upperX) = window;
         
     end
     mask = imfill(UpdatedMask, 'holes');    
